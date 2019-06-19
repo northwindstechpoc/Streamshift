302453f7e496f2fe04cc5d89ed15de7f2f4424b7
Troy Sellers <tsellers@salesforce.com>
2019-06-20 09:39:31 +1000

    heroku

--- src/main/java/com/salesforce/emp/connector/example/LoginExample.java
+++ src/main/java/com/salesforce/emp/connector/example/LoginExample.java
@@ -8,15 +8,16 @@ package com.salesforce.emp.connector.example;
 
 import static com.salesforce.emp.connector.LoginHelper.login;
 
-import java.net.URL;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
+
 import org.eclipse.jetty.util.ajax.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.salesforce.emp.connector.BayeuxParameters;
 import com.salesforce.emp.connector.EmpConnector;
-import com.salesforce.emp.connector.LoginHelper;
 import com.salesforce.emp.connector.TopicSubscription;
 
 /**
@@ -24,22 +25,30 @@ import com.salesforce.emp.connector.TopicSubscription;
  *
  * @author hal.hildebrand
  * @since API v37.0
+ * 
+ * updated for Heroku deploy troy.sellers
+ * 
  */
 public class LoginExample {
+	
+	private static Logger LOG = LoggerFactory.getLogger(LoginExample.class);
     public static void main(String[] argv) throws Exception {
-        if (argv.length < 3 || argv.length > 4) {
-            System.err.println("Usage: LoginExample username password topic [replayFrom]");
+    	
+        if (null == System.getenv("SF_USER") || null == System.getenv("SF_PASS") || null == System.getenv("SF_TOPIC")) {
+            LOG.error("Usage: Set SF_USER, SF_PASS and SF_TOPIC as environment variables to run");
             System.exit(1);
-        }
+        } 
+    	
         long replayFrom = EmpConnector.REPLAY_FROM_EARLIEST;
-        if (argv.length == 4) {
-            replayFrom = Long.parseLong(argv[3]);
+        if (null != System.getenv("SF_REPLAY_FROM")) {
+            replayFrom = Long.parseLong(System.getenv("SF_REPLAY_FROM"));
         }
 
         BearerTokenProvider tokenProvider = new BearerTokenProvider(() -> {
             try {
-                return login(argv[0], argv[1]);
+                return login(System.getenv("SF_USER"), System.getenv("SF_PASS"));
             } catch (Exception e) {
+            	LOG.error(e.getMessage());
                 e.printStackTrace(System.err);
                 System.exit(1);
                 throw new RuntimeException(e);
@@ -56,8 +65,8 @@ public class LoginExample {
 
         connector.start().get(5, TimeUnit.SECONDS);
 
-        TopicSubscription subscription = connector.subscribe(argv[2], replayFrom, consumer).get(5, TimeUnit.SECONDS);
+        TopicSubscription subscription = connector.subscribe(System.getenv("SF_TOPIC"), replayFrom, consumer).get(5, TimeUnit.SECONDS);
 
-        System.out.println(String.format("Subscribed: %s", subscription));
+        LOG.info(String.format("Subscribed: %s", subscription));
     }
 }
